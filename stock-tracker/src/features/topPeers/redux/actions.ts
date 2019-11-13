d870913e323922cb6c1eeac501520d973fd16d37
AnnaKanska <ann.kanska@gmail.com>
2019-11-13 10:11:25 +0100

    fixed top peers

--- stock-tracker/src/features/topPeers/redux/actions.ts
+++ stock-tracker/src/features/topPeers/redux/actions.ts
@@ -11,13 +11,17 @@ export type AddTopPeers = ActionPayload<typeof ADD_TOP_PEERS, TopPeersType>;
 export type SetLoading = Action<typeof SET_LOADING_PEERS>;
 export type SetTopPeersError = Action<typeof SET_ERROR_PEERS>;
 
-export const addTopPeersAction = (topPeers: TopPeersType) => ({
+export const addTopPeersAction = (topPeers: TopPeersType): AddTopPeers => ({
   type: ADD_TOP_PEERS,
   payload: topPeers
 });
 
-export const setLoadingPeersAction = () => ({ type: SET_LOADING_PEERS });
+export const setLoadingPeersAction = (): SetLoading => ({
+  type: SET_LOADING_PEERS
+});
 
-export const setErrorPeersAction = () => ({ type: SET_ERROR_PEERS });
+export const setErrorPeersAction = (): SetTopPeersError => ({
+  type: SET_ERROR_PEERS
+});
 
 export type TopPeersActions = AddTopPeers | SetLoading | SetTopPeersError;
